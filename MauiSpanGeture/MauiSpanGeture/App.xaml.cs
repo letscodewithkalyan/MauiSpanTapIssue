36d8b89d3e8d5d676ba1207f1257799b8a9398b1
Kalyan Pidugu <pidugukalyan@gmail.com>
2024-05-17 14:35:46 +0530

    Tap gesture

---
+++ MauiSpanGeture/MauiSpanGeture/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiSpanGeture;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ MauiSpanGeture/MauiSpanGeture/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace MauiSpanGeture;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    async void TapGestureRecognizer_Tapped(System.Object sender, Microsoft.Maui.Controls.TappedEventArgs e)
+    {
+        await App.Current.MainPage.DisplayAlert(string.Empty, "Arabic Text tapped", "Ok");
+    }
+
+    async void EnglishGestureRecognizer_Tapped(System.Object sender, Microsoft.Maui.Controls.TappedEventArgs e)
+    {
+        await App.Current.MainPage.DisplayAlert(string.Empty, "English Text tapped", "Ok");
+    }
+}
+
+
